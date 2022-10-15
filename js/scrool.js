2e22229d657924ee887870e10230a987353445e4
dev-brunosantos <100520104+dev-brunosantos@users.noreply.github.com>
2022-10-15 13:25:23 -0300

    Novidades

---
+++ js/scrool.js
@@ -0,0 +1,30 @@
+ScrollReveal({ reset: true });
+    
+ScrollReveal().reveal('.sobre-texto', {
+    delay: 500,
+    origin: 'left'
+});
+ScrollReveal().reveal('.nossos-servicos', { 
+    delay: 500,
+    rotate: {
+        x: 100,
+        y: 0,
+        z: 0
+    } 
+});
+
+ScrollReveal().reveal('.parallax', {
+    delay: 1000,
+    opacity: 0
+});
+
+ScrollReveal().reveal('.cartoes', { 
+    delay: 1000,
+    origin: 'left',
+    opacity: 0,
+    rotate: {
+        x: 100,
+        y: 0,
+        z: 0
+    }
+});
